1964dbcf0ff6912fa7a2cf0bf713388cb21720ee
matsuno1818 <matsuno.812@gmail.com>
2020-08-12 20:14:22 +0900

    devise_parameter_sanitizerの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,19 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:password])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:familyname_reading])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:firstname_reading])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:birthday])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:email])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:password])
+  end
 end
