2933f24bd562c36df309475ebafb3a46758da31f
matsuno1818 <matsuno.812@gmail.com>
2020-08-13 11:32:19 +0900

    registration_controllerのアクション設定

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,33 +1,35 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-   before_action :configure_sign_up_params, only: [:create]
-  #before_action :configure_account_update_params, only: [:update]
+   #before_action :configure_sign_up_params, only: [:create]
+   before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
-  # def new
-  #    super
-  #  end
+   def new
+      super
+    end
 
   # POST /resource
    def create
      super
+
    end
 
   # GET /resource/edit
-  # def edit
-  #   super
-  # end
+   def edit
+
+     super
+   end
 
   # PUT /resource
-  # def update
-  #   super
-  # end
+   def update
+     super
+   end
 
   # DELETE /resource
-  # def destroy
-  #   super
-  # end
+   def destroy
+     super
+   end
 
   # GET /resource/cancel
   # Forces the session data which is usually expired after sign
@@ -38,12 +40,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+   #protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  #  def configure_sign_up_params
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :familyname_reading, :firstname_reading, :birthday])
+
+  #  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
