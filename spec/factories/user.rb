bc4af9314e5a3dbbaa313d4c51d48d879e58a8fb
matsuno1818 <matsuno.812@gmail.com>
2020-08-14 20:26:52 +0900

    テストコード

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,16 +1,14 @@
 FactoryBot.define do
   factory :user do
     nickname { 'abe' }
-    email {Faker::Internet.free_email}
+    email { Faker::Internet.free_email }
     password = Faker::Internet.password(min_length: 6)
-    password {password}
-    password_confirmation {password}
+    password { password }
+    password_confirmation { password }
     family_name { '阿部' }
     first_name { '太郎' }
-    failyname_reading { 'アベ' }
+    familyname_reading { 'アベ' }
     firstname_reading { 'タロウ' }
-    birthday_year { 1 }
-    birthday_month { 1 }
-    birthday_date { 1 }
+    birthday { '1931-01-01' }
   end
 end
