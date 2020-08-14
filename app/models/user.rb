659e8a5bba8e3dde1c8fb70f38f0502b5152372e
matsuno1818 <matsuno.812@gmail.com>
2020-08-14 20:25:46 +0900

    バリデーションのメッセージ削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
-  with_options presence: true do
-    validates :email, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: "一意性で@、半角英数字で入力してください。"}
-    validates :password, format: { with: /\A[a-z0-9]+\z{6,}/i, message: "半角英数字で６文字以上で入力してください。"}
-    validates :family_name, :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角で入力してください。"}
-    validates :familyname_reading, :firstname_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力してください。"}
-    
-  end
 
+  with_options presence: true do
+    validates :nickname
+    validates :email, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+    validates :password, format: { with: /\A[a-z0-9]+\z{6,}/i }
+    validates :family_name, :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :familyname_reading, :firstname_reading, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :birthday
+  end
 end
