dbc8827b7a43dc6f1a6358a52db9ed8ba946cb64
matsuno1818 <matsuno.812@gmail.com>
2020-08-15 18:49:44 +0900

    正規表現のインスタンス変数の設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+
+    @nickname = /\A[ぁ-んァ-ン一-龥a-z0-9]+\z/i 
+    @email = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    @password = /\A[a-z0-9]+\z{6,}/i
+    @zenkaku = /\A[ぁ-んァ-ン一-龥]/
+    @zenkakukana = /\A[ァ-ヶー－]+\z/
+  
+
   with_options presence: true do
-    validates :nickname
-    validates :email, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-    validates :password, format: { with: /\A[a-z0-9]+\z{6,}/i }
-    validates :family_name, :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-    validates :familyname_reading, :firstname_reading, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :nickname, uniqueness: true, format: { with: @nickname }
+    validates :email, uniqueness: true, format: { with: @email }
+    validates :password, format: { with: @password, message: "Password Include both letters and numbers" }
+    validates :family_name, :first_name, format: { with: @zenkaku, message: "Full-width characters" }
+    validates :familyname_reading, :firstname_reading, format: { with: @zenkakukana, message: " Full-width katakana characters" }
     validates :birthday
   end
 end
