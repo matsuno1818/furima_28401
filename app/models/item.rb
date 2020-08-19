1725574fd2977c44629bf492453af01462a9b4e0
matsuno1818 <matsuno.812@gmail.com>
2020-08-19 15:52:55 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,18 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  # with_options presence: true do
-  #   validates :image, :name, :text, :genre, :state, :delivery, :prefecture, :shipment, :price
-  # end
+  # @minimum_price = 300
+  # @maximum_price = 9999999
+  @half_width_number = /\A[a-z]+\z/
+
+  with_options presence: true do
+    validates :image, :name, :text, :genre, :state, :delivery, :prefecture, :shipment
+    validates :price, numericality: { greater_than_or_equal_to: 300, message: "Price Out of setting range" }
+    validates :price, numericality: { less_than_or_equal_to: 9999999, messsage: "Price Out of setting range" }
+    validates :price, format: {with: @half_width_number, message: "Price Half-width number"}
+  #   validates :name, presence: {
+  #   message: I18n.t('activemodel.errors.models.user.fullname_blank')
+  # }
+# end
+  end
 end 
