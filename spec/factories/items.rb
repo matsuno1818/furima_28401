8108111f97777175462c82fd6052d78310d69d8b
matsuno1818 <matsuno.812@gmail.com>
2020-08-19 21:34:11 +0900

    Itemモデルのテストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :item do
+    text {'美味しいチーズケーキ'}
+    name  { 'ケーキ' }
+    genre { 1 }
+    state { 1 }
+    delivery { 1 }
+    prefecture { 1 }
+    shipment { 1 }
+    price { 1000 }
     
   end
 end
+
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,69 @@
 require 'rails_helper'
-
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '商品出品情報の保存' do
+    before do
+      @items = FactoryBot.build(:item)
+      @items.image = fixture_file_upload('public/images/test_image.png')
+    end
+    
+    it 'すべての値が正しく入力されていれば保存できること' do
+      expect(@items).to be_valid
+    end
+    it '画像が空では保存できないこと' do
+      @items.image = nil
+      @items.valid?
+      expect(@ites.errors.full_messages).to include("Image can't be blank")
+    end
+    it '商品名が空では保存できないこと' do
+      @items.name = nil
+      @items.valid?
+      expect(@ites.errors.full_messages).to include("Name can't be blank")
+    end
+    it '商品の説明が空では保存できないこと' do
+      @items.text = nil
+      @items.valid?
+      expect(@ites.errors.full_messages).to include("Text can't be blank")
+    end
+    it 'カテゴリーの選択をしないと保存できないこと' do
+      @items.genre = nil
+      @items.valid?
+      expect(@ites.errors.full_messages).to include("Genre can't be blank")
+    end
+    it '商品の状態について選択しないと保存できないこと' do
+      @items.state = nil
+      @items.valid?
+      expect(@ites.errors.full_messages).to include("State can't be blank")
+    end
+    it '送料の負担について選択しないと保存できないこと' do
+      @items.delivery = nil
+      @items.valid?
+      expect(@ites.errors.full_messages).to include("Delivery can't be blank")
+    end
+    it '配送元の地域を選択しないと保存できないこと' do
+      @items.prefecture = nil
+      @items.valid?
+      expect(@ites.errors.full_messages).to include("Prefecture can't be blank")
+    end
+    it '発送までの日数を選択しないと保存できないこと' do
+      @items.shipment = nil
+      @items.valid?
+      expect(@ites.errors.full_messages).to include("Shipment can't be blank")
+    end
+    it '価格が空では保存できないこと' do
+      @items.price = nil
+      @items.valid?
+      expect(@ites.errors.full_messages).to include("Price can't be blank")
+    end
+    it '価格が¥300未満では保存できないこと' do
+      @items.price = 299
+      @items.valid?
+      binding.pry
+      expect(@ites.errors.full_messages).to include("Price Price Out of setting range")
+    end
+    it '価格が¥9,999,999を超過すると保存できないこと' do
+      @items.price = 10000000
+      @items.valid?
+      expect(@ites.errors.full_messages).to include("Price must be less than or equal to 9999999")
+    end
+ end
 end
