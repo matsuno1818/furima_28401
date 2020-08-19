63ee66cb9a38d89cea9e08c65052c6f4ade06791
matsuno1818 <matsuno.812@gmail.com>
2020-08-19 21:34:46 +0900

    バリデーションメッセージ変更予定

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,14 +6,18 @@ class Item < ApplicationRecord
   # @maximum_price = 9999999
   @half_width_number = /\A[a-z]+\z/
 
+
   with_options presence: true do
     validates :image, :name, :text, :genre, :state, :delivery, :prefecture, :shipment
     validates :price, numericality: { greater_than_or_equal_to: 300, message: "Price Out of setting range" }
     validates :price, numericality: { less_than_or_equal_to: 9999999, messsage: "Price Out of setting range" }
     validates :price, format: {with: @half_width_number, message: "Price Half-width number"}
-  #   validates :name, presence: {
-  #   message: I18n.t('activemodel.errors.models.user.fullname_blank')
-  # }
-# end
+  
+  #   # メッセージを直書きした場合
+  # validates :name, presence: { message: "must be given please" }
+
+  # # 動的な属性値を含むメッセージの場合。%{value}は実際の属性値に
+  # # 置き換えられる。%{attribute}や%{model}も利用可能。
+  # validates :age, numericality: { message: "%{value} seems wrong" }
   end
 end 
