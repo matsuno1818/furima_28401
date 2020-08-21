e347b67da22809089e3d12a5364534820ff53dbc
matsuno1818 <matsuno.812@gmail.com>
2020-08-21 19:29:02 +0900

    edit,updateアクションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,6 +25,12 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
   end
   
   def destroy
