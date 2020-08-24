63e4a70d60203147a782f6d7c7ec7204b9699b0c
matsuno1818 <matsuno.812@gmail.com>
2020-08-24 14:22:34 +0900

    payjp導入

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,6 +4,25 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @order = Order.new
+    if @order.create
+      pay_item
+      return redirect_to root_path
+    else
+      render 'index'
+    end
   end
   
+  private
+
+  def order_params
+    params.permit(:token)
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"] # PAY.JPテスト秘密鍵
+    Payjp::Charge.create(
+      card: order_params[:token],    # カードトークン  
+  end
+
 end
