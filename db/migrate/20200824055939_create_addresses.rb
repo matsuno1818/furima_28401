48c517800ad2a635d1cd1f0496157a216f1c6301
matsuno1818 <matsuno.812@gmail.com>
2020-08-25 20:07:35 +0900

    migarationの修正

--- db/migrate/20200824055939_create_addresses.rb
+++ db/migrate/20200824055939_create_addresses.rb
@@ -6,8 +6,8 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
       t.string :city,           null: false
       t.string :house_number,   null: false
       t.string :building_name,  default: ""
-      t.string :phone_namber,   null: false
-      t.references :item,       null: false,  foreign_key: true
+      t.string :phone_number,   null: false
+      t.references :order,       null: false,  foreign_key: true
       t.timestamps
     end
   end
