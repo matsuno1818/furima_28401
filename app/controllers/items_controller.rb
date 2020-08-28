85155c7bd342d8cee404d8b016c3ebb9889615b0
matsuno1818 <matsuno.812@gmail.com>
2020-08-28 16:07:43 +0900

    items_controllerの不要な記述の削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  # skip_before_action :authenticate_user!, only:[:index, :show]
+  
   before_action :move_to_index,except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
 
