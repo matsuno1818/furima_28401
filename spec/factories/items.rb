aadbcf6a9af47b1dd5753f35d244de9240ce2400
matsuno1818 <matsuno.812@gmail.com>
2020-08-28 16:05:02 +0900

    itemsのFactoryBotの修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,11 +2,11 @@ FactoryBot.define do
   factory :item do
     text {'美味しいチーズケーキ'}
     name  { 'ケーキ' }
-    genre { 1 }
-    state { 1 }
-    delivery { 1 }
-    prefecture { 1 }
-    shipment { 1 }
+    genre_id { 1 }
+    state_id { 1 }
+    delivery_id { 1 }
+    prefecture_id { 1 }
+    shipment_id { 1 }
     price { 1000 }
     association :user
 
