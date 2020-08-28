57497f94780f71e5437fa0b6ce265c3b312999e1
matsuno1818 <matsuno.812@gmail.com>
2020-08-28 16:10:13 +0900

    item_orderのsaveの修正

--- app/forms/item_order.rb
+++ app/forms/item_order.rb
@@ -14,7 +14,7 @@ class ItemOrder
   
   def save
     # クレジットの情報を保存
-    order = Order.create(user_id: user_id, item_id: item_id, tolken: token)
+    order = Order.create( user_id: user_id, item_id: item_id)
     # 住所の情報を保存
     Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
   end
