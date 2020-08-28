85b87ef6f2166c0d6f2f307e9ce049feda030077
matsuno1818 <matsuno.812@gmail.com>
2020-08-28 16:02:10 +0900

    order_controllerの設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,17 @@
 class OrdersController < ApplicationController
 
+before_action :item_params, only: [:create]
+
+
   def index
     @item = Item.find(params[:item_id])
     @order = ItemOrder.new
-    
+    if current_user.id == @item.user_id
+      redirect_to root_path
+    end
+    if @item.order != nil
+      redirect_to root_path
+    end
   end
 
   def new
@@ -11,12 +19,13 @@ class OrdersController < ApplicationController
   end
 
   def create
-  
     @order = ItemOrder.new(order_params)
     if @order.valid?
+      pay_item
       @order.save  
       return redirect_to root_path
     else
+      @item = Item.find(params[:item_id])
       render "index"    
     end
   end
@@ -27,10 +36,16 @@ class OrdersController < ApplicationController
     params.require(:item_order).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :token).merge(item_id: params[:item_id], user_id: current_user.id)
   end
 
+  def item_params
+    @item = Item.find(params[:item_id])
+  end
+
   def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"] # PAY.JPテスト秘密鍵
     Payjp::Charge.create(
-      card: order_params[:token]  
+      amount: @item.price,
+      card: order_params[:token],  
+      currency:'jpy' 
       )
   end
 
