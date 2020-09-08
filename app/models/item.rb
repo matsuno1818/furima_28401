8a33bf5406e44188cfe5e1096b1dc0029268993e
matsuno1818 <matsuno.812@gmail.com>
2020-09-08 23:07:57 +0900

    複数枚投稿のアソシエーション変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,7 @@ class Item < ApplicationRecord
   belongs_to_active_hash :delivery
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :shipment
-  has_one_attached :image
+  has_many_attached :images
 
   belongs_to :user
   has_one :order
