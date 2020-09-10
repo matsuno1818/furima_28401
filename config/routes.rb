f4822998fff76eaca972ba7e473b33403021da53
matsuno1818 <matsuno.812@gmail.com>
2020-09-10 12:47:16 +0900

    マイページの追加実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
 
+  get 'users/show'
   devise_for :users
   
   root to: "items#index"
   resources :items do
    resources :orders, only:[:create, :index, :new]
   end
+  resources :users, only: [:show, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
