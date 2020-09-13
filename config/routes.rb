8aaf984fcbf77e607d78509c1600536f173c3aa3
matsuno1818 <matsuno.812@gmail.com>
2020-09-13 12:10:08 +0900

    マイページの削除

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,5 @@ Rails.application.routes.draw do
   resources :items do
    resources :orders, only:[:create, :index, :new]
   end
-  resources :users, only: [:show, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
